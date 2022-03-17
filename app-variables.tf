499c19fb1a71bbd6b78bfc594b93439000474f55
mohit.1stop <mohit.1stop@gmail.com>
2022-03-17 18:01:05 +0530

    add service principal variables

--- app-variables.tf
+++ app-variables.tf
@@ -98,3 +98,24 @@ variable "az_sp" {
   description = "Azure Service plan where the App service will be created"
 }
 
+# Service Principal Variables
+
+variable "client_id" {
+    description =   "Client ID (APP ID) of the application"
+    type        =   string
+}
+
+variable "client_secret" {
+    description =   "Client Secret (Password) of the application"
+    type        =   string
+}
+
+variable "subscription_id" {
+    description =   "Subscription ID"
+    type        =   string
+}
+
+variable "tenant_id" {
+    description =   "Tenant ID"
+    type        =   string
+}

663a773da15a3450589c4534af98e748b61f7cf7
mohitonestop <101713151+mohitonestop@users.noreply.github.com>
2022-03-17 18:49:53 +0530

    Update provider-main.tf

--- provider-main.tf
+++ provider-main.tf
@@ -27,9 +27,4 @@ backend "azurerm" {
 
 provider "azurerm" {
   features {}
-
-  subscription_id   = "${env.ARM_SUBSCRIPTION_ID}"
-  tenant_id         = "${env.ARM_TENANT_ID}"
-  client_id         = "${env.ARM_CLIENT_ID}"
-  client_secret     = "${env.ARM_CLIENT_SECRET}"
 }
